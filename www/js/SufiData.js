04d3b9c05e5f4816c1f3b8c04c1061fa31d8f8b4
Marcel Timmerman <mt1957@gmail.com>
2018-05-22 22:22:59 +0200

    -track points noted -track bounds set -gpx dom tree created -gpx serialized to text

--- www/js/SufiData.js
+++ www/js/SufiData.js
@@ -141,6 +141,7 @@ var SufiData = {
       var shortestLength = 41000000;
       var currentLongitude = position.coords.longitude;
       var currentLatitude = position.coords.latitude;
+console.log('New cl: ' + currentLongitude + ', ' + currentLatitude);
       var closestPosition = [ ];
       for( var i = 0; i < points.length; i++) {
 
@@ -158,7 +159,7 @@ var SufiData = {
       }
 
       if( typeof closestPosition === 'undefined' ) {
-        console.warn('undefined closest position should not happen');
+        console.log('undefined closest position: should not happen');
       }
 
       else {
@@ -167,7 +168,7 @@ var SufiData = {
           currentLongitude, currentLatitude
         );
 
-        console.warn(
+        console.log(
           "Distance between current and closest is " +
           (d/1000).toFixed(3) + " km"
         );
@@ -204,5 +205,259 @@ var SufiData = {
     var d = R * c;
 
     return d;
+  },
+
+  //----------------------------------------------------------------------------
+  // variables used by tracking
+  trackStarted: false,
+  trackTempStopped: true,
+  trackBoundaries: [ [ 360, 360], [ 0, 0]], // min(lon/lat) max(lon/lat)
+  trackGps: [ ],
+  trackDom: null,
+
+  //----------------------------------------------------------------------------
+  doStartTrack: function ( ) {
+
+    if( SufiData.trackStarted ) {
+      console.log("Track already started, save before starting a new one");
+    }
+
+    else {
+      SufiData.trackStarted = true;
+      SufiData.trackTempStopped = false;
+      console.log("Track is started");
+
+      SufiData.center.observers.subscribe(
+        'currentLocation', SufiData, 'trackLocation'
+      );
+    }
+  },
+
+  //----------------------------------------------------------------------------
+  doSaveTrack: function ( ) {
+
+    if( SufiData.trackStarted ) {
+      console.log("Track saved");
+      SufiData.trackStarted = false;
+      SufiData.trackTempStopped = true;
+
+      SufiData.center.observers.unsubscribe(
+        'currentLocation', SufiData, 'trackLocation'
+      );
+
+      SufiData.saveTrack();
+    }
+
+    else {
+      console.log("No track started, nothing to save");
+    }
+  },
+
+  //----------------------------------------------------------------------------
+  doStopTrack: function ( ) {
+
+    if( SufiData.trackStarted ) {
+      if( SufiData.trackTempStopped ) {
+        console.log("Track is already stopped temporarely");
+      }
+
+      else {
+        console.log("Track temporarely stopped");
+        SufiData.trackTempStopped = true;
+
+        SufiData.center.observers.unsubscribe(
+          'currentLocation', SufiData, 'trackLocation'
+        );
+      }
+    }
+
+    else {
+      console.log("No track started, cannot stop");
+    }
+  },
+
+  //----------------------------------------------------------------------------
+  doContTrack: function ( ) {
+
+    if( SufiData.trackStarted ) {
+      if( SufiData.trackTempStopped ) {
+        console.log("Continue tracking");
+        SufiData.trackTempStopped = false;
+
+        SufiData.center.observers.subscribe(
+          'currentLocation', SufiData, 'trackLocation'
+        );
+      }
+
+      else {
+        console.log("Tracking already continued");
+      }
+    }
+
+    else {
+      console.log("No track started, cannot continue");
+    }
+  },
+
+  //----------------------------------------------------------------------------
+  trackLocation: function ( position ) {
+
+    var lon = position.coords.longitude;
+    var lat = position.coords.latitude;
+
+console.log("Store track GPS: " + lon + ", " + lat);
+    // save location
+    SufiData.trackGps.push( [ lon, lat]);
+//TODO add elevation as 3rd item
+//TODO add time as 4th item
+
+    // change bounding box
+    if( SufiData.trackBoundaries[0][0] > lon ) {
+      SufiData.trackBoundaries[0][0] = lon;
+    }
+
+    if( SufiData.trackBoundaries[0][1] > lat ) {
+      SufiData.trackBoundaries[0][1] = lat;
+    }
+
+    if( SufiData.trackBoundaries[1][0] < lon ) {
+      SufiData.trackBoundaries[1][0] = lon;
+    }
+
+    if( SufiData.trackBoundaries[1][1] < lat ) {
+      SufiData.trackBoundaries[1][1] = lat;
+    }
+
+    // show on map
+  },
+
+  //----------------------------------------------------------------------------
+  saveTrack: function ( ) {
+    SufiData.mkTrackXml();
+    SufiData.saveTrackXml();
+
+    // Send to site, url with username
+  },
+
+  //----------------------------------------------------------------------------
+  // convert data to xml dom tree
+  mkTrackXml: function ( ) {
+    var doc = document.implementation.createDocument( "", "", null);
+
+    // root element and attributes
+    var g = doc.createElement('gpx');
+    doc.appendChild(g);
+    g.setAttribute( 'xsi:schemaLocation', 'http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd');
+    g.setAttribute( 'xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
+    g.setAttribute( 'version', '1.1');
+    g.setAttribute( 'creator', 'Sufi Trail App');
+    g.setAttribute( 'xmlns', 'http://www.topografix.com/GPX/1/1');
+
+    // metadata section
+    var m = doc.createElement('metadata');
+    g.appendChild(m);
+
+    var me = doc.createElement('link');
+    m.appendChild(me);
+    me.setAttribute( 'href', 'http://www.sufitrail.com/');
+    me.innerHtml = 'Sufi Train Personal Hike';
+
+    me = doc.createElement('keywords');
+    m.appendChild(me);
+    me.innerHtml = 'Istanbul,Konya,Totaal,Turkije,hike';
+
+    me = doc.createElement('copyright');
+    m.appendChild(me);
+    me.innerHtml = 'Sufi Trail';
+
+    me = doc.createElement('author');
+    m.appendChild(me);
+    me.innerHtml = 'Hiker';
+//TODO name of user must be known
+
+    me = doc.createElement('description');
+    m.appendChild(me);
+    me.innerHtml = 'Personally walked track along the Sufi Trail';
+
+    me = doc.createElement('name');
+    m.appendChild(me);
+    me.innerHtml = 'Some name set to the gpx route';
+//TODO some other personalization thingy
+
+    me = doc.createElement('time');
+    m.appendChild(me);
+    me.innerHtml = (new Date).toISOString();
+
+    me = doc.createElement('bounds');
+    m.appendChild(me);
+    me.setAttribute( 'minlon', SufiData.trackBoundaries[0][0]);
+    me.setAttribute( 'minlat', SufiData.trackBoundaries[0][1]);
+    me.setAttribute( 'maxlon', SufiData.trackBoundaries[1][0]);
+    me.setAttribute( 'maxlat', SufiData.trackBoundaries[1][1]);
+
+    // track segments
+    var t = doc.createElement('trk');
+    g.appendChild(t);
+    var n = doc.createElement('name');
+    t.appendChild(n);
+//TODO some other personalization thingy
+//TODO add tracks to the same file?
+    n.innerHtml = 'Some name set to the gpx route segment';
+
+    var ts = doc.createElement('trkseg');
+    t.appendChild(ts);
+
+    for( var i = 0; i < SufiData.trackGps.length; i++) {
+      var tse = doc.createElement('trkseg');
+      ts.appendChild(tse);
+      tse.setAttribute( 'lon', SufiData.trackGps[i][0]);
+      tse.setAttribute( 'lat', SufiData.trackGps[i][1]);
+    }
+
+    SufiData.trackDom = doc;
+  },
+
+  //----------------------------------------------------------------------------
+  // save xml dom tree to disk
+  saveTrackXml: function ( ) {
+
+    var s = new XMLSerializer();
+    var xmlString = s.serializeToString(SufiData.trackDom);
+console.log(xmlString);
+
+/*
+    window.resolveLocalFileSystemURL(
+      'file:///Downloads',
+      function( dir ) {
+console.log("got main dir: " + dir);
+  		  dir.getFile(
+          '/u.gpx',
+          {create: true},
+          function ( file ) {
+console.log("got the file" + file);
+            file.createWriter(
+              function ( fileWriter ) {
+                var blob = new Blob(
+                  [xmlString],
+                  {type: 'application/gpx+xml'}
+                );
+
+                fileWriter.write(blob);
+console.log("GPX text written");
+              }, fail
+            );
+          }
+        );
+      }
+    );
+*/
+/*
+    var file = new File(
+      [xmlString], "./user-tracks/u.gpx", {type: "application/gpx+xml"}
+    );
+    var stream = nsIScriptableIO.newOutputStream( file, "text");
+    stream.writeString(xmlString);
+    stream.close();
+*/
   }
 }
