f6a371ea0726fe4eed0201276817c0280d89b7d3
Marcel Timmerman <mt1957@gmail.com>
2018-10-06 10:21:52 +0200

    provider code in java

--- Qt/Sufitrail/android/src/utils/TDAndroidUtils.java
+++ Qt/Sufitrail/android/src/utils/TDAndroidUtils.java
@@ -9,14 +9,21 @@ import android.util.Log;  // temporary for debugging
 import android.content.Intent;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.ContentProvider ;
+import android.content.ContentResolver;
+
+import android.database.MatrixCursor;
+import android.database.Cursor;
 
 import android.os.Parcelable;
-import android.os.Bundle;
+
+//import android.os.Bundle;
+//import android.os.Binder;
+//import android.os.IBinder;
 
 //import java.io.File;
 //import android.net.Uri;
-//import android.content.ContentResolver;
-//import android.database.Cursor;
+////import android.database.Cursor;
 //import android.provider.MediaStore;
 //import java.io.FileNotFoundException;
 //import java.io.IOException;
@@ -32,54 +39,107 @@ import android.os.Bundle;
 //import android.content.Context;
 //import android.os.Build;
 
-/*
-
-  ComponentName( package, class)
-  Take package name from the android manifest package element of the target
-  where the action is directed to.
-
-  For the class name, take the name from the action which must be performed
-
-
-  E.g.
-  1) Start the HikingCompanion app from another app;
-     package name = io.martimm.github.HikingCompanion
-     class name = org.qtproject.qt5.android.bindings.QtApplication
-  2) If we want to send data to another java method in the HikingCompanion app
-     we take the package name from manifest again and add the java package name
-     to it (depend on how it is defined in the activity);
-     package name = io.martimm.github.HikingCompanion
-     class name = io.martimm.github.HikingCompanion.utils.HCAndroidUtils
-
-
-  For all this to work the manifest must have an activity defined like this;
-  <manifest package="io.martimm.github.HikingCompanion" ...>
-    <application ...>
-      <activity android:name="org.qtproject.qt5.android.bindings.QtApplication">
-        <intent-filter>
-          <action android:name="android.intent.action.MAIN"/>
-          <category android:name="android.intent.category.LAUNCHER"/>
-          <data android:mimeType="* /*"/>
-        </intent-filter>
-        ...
-      </activity>
-      ...
-
-      <activity android:name=".utils.HCAndroidUtils">
-        <intent-filter>
-          <action android:name="android.intent.action.SEND"/>
-          <category android:name="android.intent.category.DEFAULT"/>
-          <data android:mimeType="* /*"/>
-        </intent-filter>
-        ...
-      </activity>
-      ...
-    </application>
-  <manifest>
-*/
 
 // ----------------------------------------------------------------------------
+public class TDAndroidUtils extends ContentProvider {
+
+  // --------------------------------------------------------------------------
+  private static String LOG_TAG = "Sufitrail TDAndroidUtils";
+
+  private static MatrixCursor _dataPathOnly = new MatrixCursor(["path"]);
+
+  // --------------------------------------------------------------------------
+  // Defined in the main.cpp file as an entry into the c++ code
+  public static native String getDataRootDir();
+
+  // --------------------------------------------------------------------------
+  @Override
+  public void onCreate ( ) {
+    super.onCreate();
+    Log.v( LOG_TAG, "in onCreate");
+  }
+
+  // --------------------------------------------------------------------------
+  @Override
+  public Cursor query (
+    Uri uri,
+    String[] projection,
+    Bundle queryArgs,
+    CancellationSignal cancellationSignal
+  ) {
+    Log.v( LOG_TAG, "in query");
+
+    // Place data in dataRootDir
+    TDAndroidUtils.moveDataPublic();
+
+    // Then setup the row in the table (one column in one row) holding
+    // the path to this data
+    if ( dataPathOnly.getCount() == 0 ) {
+      dataPathOnly.addRow([TDAndroidUtils.getDataRootDir()]);
+    }
+
+    return dataPathOnly;
+  }
+
+  // --------------------------------------------------------------------------
+  @Override
+  public Uri insert (
+    Uri uri,
+    ContentValues values
+  ) {
+    Log.v( LOG_TAG, "in insert, ignored");
+    return null;
+  }
+
+  // --------------------------------------------------------------------------
+  @Override
+  public int update (
+    Uri uri,
+    ContentValues values,
+    String selection,
+    String[] selectionArgs
+  ) {
+    Log.v( LOG_TAG, "in update, ignored");
+    return null;
+  }
+
+  // --------------------------------------------------------------------------
+  @Override
+  public int delete (
+    Uri uri,
+    String selection,
+    String[] selectionArgs
+  ) {
+    Log.v( LOG_TAG, "in delete, ignored");
+    return null;
+  }
+
+  // --------------------------------------------------------------------------
+  @Override
+  public String getType ( Uri uri ) {
+    return "plain/text";
+  }
+
+  // --------------------------------------------------------------------------
+  @Override
+  public String getStreamTypes ( Uri uri ) {
+
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+/*
 public class TDAndroidUtils {
+  private static String LOG_TAG = "TD Service";
 
   // --------------------------------------------------------------------------
   static final int HC_INSTALL_REQUEST = 1;  // The request code
@@ -103,25 +163,26 @@ public class TDAndroidUtils {
       )
     );
 
-    Log.d("TD Java object", "Send intent prepared");
+    Log.d( LOG_TAG, "Send intent prepared");
 
     // Verify that the intent will resolve to an activity
     //if ( installIntent.resolveActivity(QtNative.activity().getPackageManager()) != null ) {
-      Log.d("TD Java object", "Start service");
+      Log.d( LOG_TAG, "Start service");
 
       try {
-        QtNative.service().bindService(installIntent);
+        //QtNative.getApplicationContext().startService(installIntent);
+        QtNative.activity().startService(installIntent);
         result = true;
       }
       catch ( Exception e ) {
-        Log.d( "TD Java object", e.getMessage());
+        Log.d( LOG_TAG, e.getMessage());
       }
 
     //  return result;
     //}
 
     //else {
-    //  Log.d("TD Java object", "Intent not resolved");
+    //  Log.d( LOG_TAG, "Intent not resolved");
     //}
 
     return result;
@@ -142,20 +203,21 @@ public class TDAndroidUtils {
             )
       );
 
-    Log.d("TD Java object", "Start intent prepared");
+    Log.d( LOG_TAG, "Start intent prepared");
 
     // Verify that the intent will resolve to an activity
     if ( startIntent.resolveActivity(QtNative.activity().getPackageManager()) != null ) {
-      Log.d("TD Java object", "Start activity");
+      Log.d( LOG_TAG, "Start activity");
 
       QtNative.activity().startActivity(startIntent);
     }
 
     else {
-      Log.d("TD Java object", "Intent not resolved");
+      Log.d( LOG_TAG, "Intent not resolved");
       return false;
     }
 
     return true;
   }
 }
+*/
